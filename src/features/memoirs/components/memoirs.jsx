c32b5dd463ad63edd2f95f2047bf48bcbf007b51
merima98 <merimaceranic98@gmail.com>
2022-08-12 17:17:14 +0200

    Added style for memoirs.

--- src/features/memoirs/components/memoirs.jsx
+++ src/features/memoirs/components/memoirs.jsx
@@ -1,20 +1,23 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Grid } from "@chakra-ui/react";
+
 import Memoir from "./memoir";
 
 const Memoirs = (props) => {
   const { memoirs } = props;
 
   return (
-    <Container>
-      {memoirs.map((memoir) => {
-        return (
-          <Memoir
-            key={memoir.id}
-            image={memoir.image}
-            content={memoir.content}
-          />
-        );
-      })}
+    <Container maxW={"auto"}>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        {memoirs.map((memoir) => {
+          return (
+            <Memoir
+              key={memoir.id}
+              image={memoir.image}
+              content={memoir.content}
+            />
+          );
+        })}
+      </Grid>
     </Container>
   );
 };
